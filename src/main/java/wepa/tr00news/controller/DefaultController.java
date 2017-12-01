ba1645c7c1d0de2c3b3e157e3c16616229ae33c2
Juurinen Juho T <juho.juurinen@gmail.com>
2017-12-01 14:05:55 +0200

    hello github

---
+++ src/main/java/wepa/tr00news/controller/DefaultController.java
@@ -0,0 +1,18 @@
+package wepa.tr00news.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultController {
+
+    @RequestMapping("*")
+    @ResponseBody
+    public String handleDefault() {
+        return "This is the water, and this is the well.<br/>" +
+                "Drink full, and descend.<br/>" +
+                "The horse is the white of the eyes, and dark within.<br/>";
+    }
+
+}
